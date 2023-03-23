94feb0f069e7c3a44eeb9d1aa10d1ef9cf10f0de
TTNhan <nhan12341184@gmail.com>
2023-03-23 08:04:30 +0700

    export as picture

--- src/assignment_csd/GraphController.java
+++ src/assignment_csd/GraphController.java
@@ -4,6 +4,7 @@
  */
 package assignment_csd;
 
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -15,6 +16,7 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 import javafx.animation.AnimationTimer;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -26,6 +28,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
@@ -33,6 +36,7 @@ import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
 
 /**
  * FXML Controller class
@@ -40,11 +44,11 @@ import javafx.stage.Stage;
  * @author TTNhan
  */
 public class GraphController {
-    
+
     ResizableCanvas canvas = new ResizableCanvas();
     GraphManagement graph = Assignment_CSD.graph;
     File saveFile;
-    
+
     @FXML
     private Pane paneCanvas;
     @FXML
@@ -62,7 +66,7 @@ public class GraphController {
 
     //gesture on canvas
     public void initialCanvas() {
-        
+
         paneCanvas.getChildren().add(canvas);
         canvas.widthProperty().bind(paneCanvas.widthProperty().subtract(2));
         canvas.heightProperty().bind(paneCanvas.heightProperty().subtract(2));
@@ -92,7 +96,7 @@ public class GraphController {
             }
             //Primary mouse button
             if (graph.clickToAddPoint && e.getButton() == MouseButton.PRIMARY) {
-                
+
             }
         });
 
@@ -170,7 +174,7 @@ public class GraphController {
             canvas.draw();
         });
     }
-    
+
     int findHoverVertexIndex(double x, double y) {
         int find = -1;
         int index = 0;
@@ -183,7 +187,7 @@ public class GraphController {
         }
         return find;
     }
-    
+
     public void initialize() {
         initialCanvas();
         colorPickerConvex.setValue(graph.convexColor);
@@ -193,19 +197,19 @@ public class GraphController {
         slider.setValue(graph.NumberOfRandomVertex);
         textFieldNNumberOfVertex.setText(String.valueOf(graph.NumberOfRandomVertex));
     }
-    
+
     @FXML
     private void pickConvexColor(ActionEvent event) {
         graph.convexColor = colorPickerConvex.getValue();
         canvas.draw();
     }
-    
+
     @FXML
     private void pickPointColor(ActionEvent event) {
         graph.pointColor = colorPickerPoint.getValue();
         canvas.draw();
     }
-    
+
     @FXML
     private void getTextInput(KeyEvent event) {
         try {
@@ -216,13 +220,13 @@ public class GraphController {
         } catch (Exception e) {
         }
     }
-    
+
     @FXML
     private void sliderNumberOfVertex(MouseEvent event) {
         graph.NumberOfRandomVertex = (int) Math.round(slider.getValue());
         textFieldNNumberOfVertex.setText(String.valueOf(graph.NumberOfRandomVertex));
     }
-    
+
     @FXML
     private void randomButton(ActionEvent event) {
         Random rand = new Random();
@@ -233,7 +237,7 @@ public class GraphController {
             int v = rand.nextInt(maxValue);
             s.add(new Point(u, v));
         }
-        
+
         ArrayList<Point> a = new ArrayList<>(s);
         String input = "";
         for (int i = 0; i < a.size(); i++) {
@@ -243,48 +247,48 @@ public class GraphController {
         graph.setPoints(a);
         canvas.resetScreen();
     }
-    
+
     @FXML
     private void textFieldNNumberOfVertex(KeyEvent event) {
         graph.NumberOfRandomVertex = Integer.parseInt(textFieldNNumberOfVertex.getText());
         slider.setValue(Double.valueOf(graph.NumberOfRandomVertex));
     }
-    
+
     @FXML
     private void resetScreen(ActionEvent event) {
         canvas.resetScreen();
     }
-    
+
     @FXML
     private void magicButton(ActionEvent event) {
         GameManagement game = new GameManagement(Assignment_CSD.graphStage);
         Stage stage = new Stage();
         Assignment_CSD.graphStage.hide();
-        
+
         game.setSize(canvas.getWidth(), canvas.getHeight());
         game.setBall(canvas.getPoint(graph.points));
         game.start(stage);
     }
-    
+
     @FXML
     private void about(ActionEvent event) throws IOException {
         Stage stage = new Stage();
         Parent root = FXMLLoader.load(getClass().getResource("Help.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.setTitle("Help");
         stage.setWidth(1200);
         stage.setHeight(900);
         stage.show();
     }
-    
+
     @FXML
     private void exit(ActionEvent event) {
         Assignment_CSD.graphStage.close();
     }
-    
+
     @FXML
     void clear() {
         graph.points.clear();
@@ -292,29 +296,29 @@ public class GraphController {
         canvas.draw();
         textInput.setText("");
     }
-    
+
     private void clear(ActionEvent event) {
         clear();
     }
-    
+
     void saveToFile(File file) {
         if (file == null) {
             return;
         }
-        
+
         try (FileWriter fileWriter = new FileWriter(file)) {
-            
+
             for (Point p : graph.points) {
                 fileWriter.write(p.getX() + " " + p.getY() + "\n");
             }
-            
+
             fileWriter.close();
         } catch (Exception ex) {
             Alert alert = new Alert(Alert.AlertType.WARNING, "There are some error when saving file!");
             alert.show();
         }
     }
-    
+
     @FXML
     private void saveAs(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
@@ -322,10 +326,10 @@ public class GraphController {
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("List of points", "*.geo"));
         fileChooser.setTitle(" Save File As ");
         File file = fileChooser.showSaveDialog(Assignment_CSD.graphStage);
-        
+
         saveToFile(file);
     }
-    
+
     @FXML
     private void save(ActionEvent event) {
         if (saveFile == null) {
@@ -335,14 +339,14 @@ public class GraphController {
             fileChooser.setTitle(" Save File ");
             saveFile = fileChooser.showSaveDialog(Assignment_CSD.graphStage);
         }
-        
+
         saveToFile(saveFile);
-        
+
         if (saveFile != null) {
             closeFile.setDisable(false);
         }
     }
-    
+
     @FXML
     private void closeFile(ActionEvent event) {
         if (saveFile != null) {
@@ -351,7 +355,7 @@ public class GraphController {
             closeFile.setDisable(true);
         }
     }
-    
+
     void updateInput() {
         String input = "";
         for (int i = 0; i < graph.points.size(); i++) {
@@ -359,19 +363,19 @@ public class GraphController {
         }
         textInput.setText(input);
     }
-    
+
     @FXML
     private void open(ActionEvent event) {
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Open Geometry File");
-        
+
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("List of points", "*.geo"));
-        
+
         File file = fileChooser.showOpenDialog(Assignment_CSD.graphStage);
-        
+
         try {
             Scanner scanner = new Scanner(file);
-            
+
             while (scanner.hasNextLine()) {
                 String s = scanner.nextLine();
                 String s1[] = s.split(" ");
@@ -381,12 +385,12 @@ public class GraphController {
                     break;
                 }
             }
-            
+
         } catch (FileNotFoundException | NumberFormatException e) {
             Alert alert = new Alert(Alert.AlertType.WARNING, "There are some error when loading file!");
             alert.show();
         }
-        
+
         if (file != null) {
             saveFile = file;
             closeFile.setDisable(false);
@@ -396,5 +400,21 @@ public class GraphController {
             canvas.draw();
         }
     }
-    
+
+    @FXML
+    private void saveAsPicture(ActionEvent event) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.initialFileNameProperty().set("Points.png");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Picture", "*.png"));
+        fileChooser.setTitle(" Save File As Picture ");
+        File file = fileChooser.showSaveDialog(Assignment_CSD.graphStage);
+
+        if (file != null) {
+            WritableImage writableImage = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+            canvas.snapshot(null, writableImage);
+            RenderedImage renderedImage = SwingFXUtils.fromFXImage(writableImage, null);
+            ImageIO.write(renderedImage, "png", file);
+        }
+    }
+
 }
